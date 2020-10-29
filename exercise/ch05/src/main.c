ceeba51b7abf7c9d0318613b79339a2b642eed42
Tin Unkai <tinunkai@gmail.com>
2020-10-29 17:16:03 +0900

    doing ch05 homework

--- exercise/ch05/src/main.c
+++ exercise/ch05/src/main.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 
 int main(int argc, char *argv[])
 {
+    int foo = 3;
+
     int pid = fork();
     if (pid < 0) {
         fprintf(stderr, "fork faile\n");
     } else if (pid == 0) {
-        printf("%d\n", pid);
+        foo = 4;
+        creat("tmp", O_CREAT);
+        printf("c: %d\n", foo);
     } else if (pid > 0) {
-        printf("%d\n", pid);
+        foo = 2;
+        printf("p: %d\n", foo);
     }
     return 0;
 }
