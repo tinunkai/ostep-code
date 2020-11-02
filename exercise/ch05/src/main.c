bfa14e44e26b53fd49adbc2c38a843ac33d85b8d
Tin Unkai <tinunkai@gmail.com>
2020-11-02 17:08:10 +0900

    trying write same file with fork

--- exercise/ch05/src/main.c
+++ exercise/ch05/src/main.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <string.h>
 
 
 int main(int argc, char *argv[])
 {
-    int foo = 3;
+    int fd = open("tmp.md", O_WRONLY|O_CREAT|O_TRUNC, 0644);
 
     int pid = fork();
     if (pid < 0) {
         fprintf(stderr, "fork faile\n");
     } else if (pid == 0) {
-        foo = 4;
-        creat("tmp", O_CREAT);
-        printf("c: %d\n", foo);
+        char *buf = "foobar\n";
+        ssize_t cnt = strlen(buf);
+        write(fd, buf, cnt);
+        printf("c: %d\n", fd);
     } else if (pid > 0) {
-        foo = 2;
-        printf("p: %d\n", foo);
+        char *buf = "barfoo\n";
+        ssize_t cnt = strlen(buf);
+        write(fd, buf, cnt);
+        printf("p: %d\n", fd);
     }
     return 0;
 }
