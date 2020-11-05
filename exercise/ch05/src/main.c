54a035f919570f92a7b86aa38f86baa023e286e8
Tin Unkai <tinunkai@gmail.com>
2020-11-05 17:00:37 +0900

    trying pipe()

--- exercise/ch05/src/main.c
+++ exercise/ch05/src/main.c
@@ -1,27 +1,72 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <string.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<string.h>
+#include<sys/wait.h>
 
-
-int main(int argc, char *argv[])
+int main()
 {
-    int fd = open("tmp.md", O_WRONLY|O_CREAT|O_TRUNC, 0644);
+    // We use two pipes
+    // First pipe to send input string from parent
+    // Second pipe to send concatenated string from child
 
-    int pid = fork();
-    if (pid < 0) {
-        fprintf(stderr, "fork faile\n");
-    } else if (pid == 0) {
-        char *buf = "foobar\n";
-        ssize_t cnt = strlen(buf);
-        write(fd, buf, cnt);
-        printf("c: %d\n", fd);
-    } else if (pid > 0) {
-        char *buf = "barfoo\n";
-        ssize_t cnt = strlen(buf);
-        write(fd, buf, cnt);
-        printf("p: %d\n", fd);
+    int fd1[2]; // Used to store two ends of first pipe
+    int fd2[2]; // Used to store two ends of second pipe
+
+    char input_str[] = "hello";
+    pid_t p;
+
+    if (pipe(fd1)==-1)
+    {
+        fprintf(stderr, "Pipe Failed" );
+        return 1;
     }
+    if (pipe(fd2)==-1)
+    {
+        fprintf(stderr, "Pipe Failed" );
+        return 1;
+    }
+
+    p = fork();
+
+    if (p < 0)
+    {
+        fprintf(stderr, "fork Failed" );
+        return 1;
+    }
+
+    // Parent process
+    else if (p > 0)
+    {
+        close(fd1[0]); // Close reading end of first pipe
+
+        // Write input string and close writing end of first
+        // pipe.
+        write(fd1[1], input_str, strlen(input_str)+1);
+        close(fd1[1]);
+
+        // Wait for child to send a string
+        wait(NULL);
+
+        close(fd2[0]);
+        close(fd2[1]);
+    }
+
+    // child process
+    else
+    {
+        close(fd1[1]); // Close writing end of first pipe
+        close(fd2[1]);
+
+        char input_str[100];
+        read(fd1[0], input_str, 100);
+
+        close(fd1[0]);
+        close(fd2[0]);
+
+        printf("%s\n", input_str);
+    }
+
     return 0;
 }
-
